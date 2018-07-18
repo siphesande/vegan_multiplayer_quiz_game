39a91b32b1ebb2b5f433df8eadbb0ba4b6be67fa
siphesande <siphesande@projectcodex.co>
2018-07-18 12:05:43 +0200

    make it better

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const serveStatic = require("serve-static")
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const pusher = new Pusher({
-  appId: '',
-  key: '',
-  secret: '',
+  appId: '547456',
+  key: 'd24f1489405006464fcf',
+  secret: 'e051a0ae5b02fe1d0548',
   cluster: 'eu',
   encrypted: true
 })
